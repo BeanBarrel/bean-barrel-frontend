e95e817f01b42acc64952dad4656bcfff3fd44db
zeezaglobal <96932409+zeezaglobal@users.noreply.github.com>
2025-09-28 18:44:37 -0500

    swdc

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -24,7 +24,7 @@ export default function HeroSection() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
         >
-          Bdfgbdfbdfbdfbdfbean Barrel
+          Bean Barrel
         </motion.h1>
 
         <motion.p
