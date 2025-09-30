49ac59e8dddc3e97cc3ec72fb61e97eb9d9de2fd
zeezaglobal <96932409+zeezaglobal@users.noreply.github.com>
2025-09-29 22:50:43 -0500

    jsdhv

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,7 +8,7 @@ export async function login(username: string, password: string) {
   const credentials = btoa(`${username}:${password}`); // base64 encode
 console.log("Tesing testing:", API_BASE_URL);
   // Test login by calling a protected endpoint
-  const res = await fetch(`${API_BASE_URL}api/secure/hello`, {
+  const res = await fetch(`api/secure/hello`, {
     headers: {
       "Authorization": `Basic ${credentials}`,
     },
